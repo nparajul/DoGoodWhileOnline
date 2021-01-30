5ab2bb56156498cbc33881dba455b3ec6be84b4f
Nitesh Parajuli <nparajul@ramapo.edu>
2021-01-30 00:05:10 -0500

    registration completed

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require("express");
 const ejs = require("ejs");
 const PORT = 3000;
 const bodyParser = require('body-parser');
-const auth = firebase.auth();
-
+var firebase = require("firebase/app");
+require("firebase/auth");
 const app = express();
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDYeycKnctIqoRn5APAltOFffR2u87EQWc",
+    authDomain: "dogoodwhile.firebaseapp.com",
+    projectId: "dogoodwhile",
+    storageBucket: "dogoodwhile.appspot.com",
+    messagingSenderId: "391295060864",
+    appId: "1:391295060864:web:9410e872d66b083f794f65",
+    measurementId: "G-PQVDJ2RE3R"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 //GET Requests
 app.get('/',(req,res)=>{
@@ -24,11 +37,33 @@ app.get('/login',(req,res)=>{
 
 //POST Requests
 
+
+
 app.post('/register', (req,res)=>{
+
+    
+      // Initialize Firebase
+    
+
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
     const password = req.body.password;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+    // Signed in 
+        res.render('login');
+  })
+  .catch((error) => {
+    
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    res.render('index');
+
+  });
+
 })
 
 app.post('/login', (req,res)=>{
